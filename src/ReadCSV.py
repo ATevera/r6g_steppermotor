259ef8f1bc47d9201b9ed809acc32309643066a5
ATevera <atevera.rz@gmail.com>
2020-11-30 19:14:35 +0000

    Add: Point Database Module

--- src/ReadCSV.py
+++ src/ReadCSV.py
@@ -1,26 +1,13 @@
 #! /usr/bin/env python3
 
-import rospy, csv, sys
+import rospy, sys
 from r6g_steppermotor.msg import CurrentPose
+from r6gPoints import PointData
 
-def GetLastPose(position):
-	"""Obtiene la última pose registrada en el archivo CSV, así como el estado del efector final para publicarlo en el tópico definido"""
-	tempPose = [0]*6
-	LastPose = [0]*6
-	rows = 1
-	with open(pathCSV, newline = '') as csvfile:
-		lector = csv.reader(csvfile, delimiter = ',', quotechar = '|')
-		for pose in lector:
-			tempPose = pose
-			rows += 1
-		for i in range(6):
-			LastPose[i] = float(tempPose[i])
-		index = 0
-	return LastPose, int(tempPose[-1]), rows
 
 def readCSV():
 	"""Inicialización de nodo y construcción de tipo de mensaje"""
-	rospy.init_node('ReadCSV',anonymous = True)
+	rospy.init_node('ReadCSV', anonymous = True)
 	pub = rospy.Publisher('CurrentPose', CurrentPose, queue_size = 100)
 	rate = rospy.Rate(5) #Frecuencia de publicación
 	r6g = CurrentPose()
@@ -30,12 +17,11 @@ def readCSV():
 	count = 0
 	sizeCSV = 0
 	while not rospy.is_shutdown():
-		r6g.position, r6g.endEffector, lineas = GetLastPose(r6g.position)
+		r6g.position, r6g.endEffector, lineas = pose.GetLast(r6g.position)
 		if sizeCSV == lineas and lineas != 2 and recurrencia == 0: 
 			pub.publish(r6g)
 			recurrencia += 1
-			with open(pathCSV, 'w') as csvfile:
-				writer = csv.writer(csvfile, delimiter = ',', quotechar = '|', quoting = csv.QUOTE_MINIMAL)
+			pose.Remake()
 		if sizeCSV == lineas:
 			recurrencia = 2
 		else: 
@@ -45,7 +31,7 @@ def readCSV():
 
 if __name__ == '__main__':
 	try:
-		pathCSV = sys.argv[1]
+		pose = PointData(sys.argv[1])
 		readCSV()
 	except rospy.ROSInterruptException:
 		pass
---
+++ src/r6gPoints.py
@@ -0,0 +1,62 @@
+#! /usr/bin/env python3
+
+import csv, math
+
+class PointData:
+	def __init__(self, path):
+		self.path = path
+
+	def Clean(self):
+		with open(self.path, 'w') as csvfile:
+			writer = csv.writer(csvfile, delimiter = ',', quotechar = '|', quoting = csv.QUOTE_MINIMAL)
+
+	def Remake(self):
+		with open(self.path, 'w') as csvfile:
+			writer = csv.writer(csvfile, delimiter = ',', quotechar = '|', quoting = csv.QUOTE_MINIMAL)
+
+	def GetLast(self, position):
+		"""Obtiene la última pose registrada en el archivo CSV, así como el estado del efector final para publicarlo en el tópico definido"""
+		tempPose = [0]*6
+		LastPose = [0]*6
+		rows = 1
+		with open(self.path, newline = '') as csvfile:
+			lector = csv.reader(csvfile, delimiter = ',', quotechar = '|')
+			for pose in lector:
+				tempPose = pose
+				rows += 1
+			for i in range(6):
+				LastPose[i] = float(tempPose[i])
+			index = 0
+		return LastPose, int(tempPose[-1]), rows
+
+	def Current(self, Target):
+		"""Verifica que la pose sea la correcta para aplicar visión artificial"""
+		tempPose = [0]*6
+		verificador = True
+		with open(self.path, newline = '') as csvfile:
+			lector = csv.reader(csvfile, delimiter = ',', quotechar = '|')
+			for pose in lector:
+				tempPose = pose
+			for i in range(6):
+				verificador = float(tempPose[i]) == Target[i] and verificador
+		return verificador
+
+	def WatchLast(self, position):
+		"""Verificar el último estado del robot en el archivo CSV para evitar sobreecribir datos"""
+		actualPose = []
+		with open(self.path, newline = '') as csvfile:
+			verificador = csv.reader(csvfile, delimiter = ',', quotechar = '|')
+			for angulos in verificador:
+				actualPose = angulos
+			index = 0
+			comparador = True
+			for joint in actualPose:
+				if index < 6: comparador = float(joint) == round(math.degrees(position[index]),4) and comparador
+				index += 1
+		if len(actualPose) == 0 : comparador = False
+		return comparador
+
+	def Write(self, values):
+		with open(self.path, 'a') as csvfile:
+			writer = csv.writer(csvfile, delimiter = ',', quotechar = '|', quoting = csv.QUOTE_MINIMAL)
+			writer.writerow(values)
